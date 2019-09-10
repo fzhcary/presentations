3ecb4f5f69a40c81de9f75724c729950fe230cb9
frank zhu <frank.zhu@gmail.com>
2019-09-10 10:44:47 -0400

    update

--- using_mock/test_hello.py
+++ using_mock/test_hello.py
@@ -7,67 +7,70 @@ import random
 # refer to https://realpython.com/python-mock-library
 class TestHello(TestCase):
     def test_greet_with_mock(self):
+        # test mock function return value
+        self.assertEqual(Hello().greet("John"), "Hello, John!")
         mock = Mock()
         mock.return_value = "hey, I am mock."
         Hello.greet = mock
-        self.assertEqual(Hello.greet(), "hey, I am mock.")
+        self.assertEqual(Hello().greet("John"), "hey, I am mock.")
 
     def test_track_call(self):
-        """ mock is very flexible """
+        # test track calls to a function
         mock = Mock()
         Hello.greet = mock
         Hello.greet("John")
         self.assertTrue(mock.called)
-        print("test_track_call:",mock.mock_calls)
         mock.assert_called_with("John")
         mock.assert_called_once_with("John")
 
+    def test_mock_without_spec(self):
+        # the deprecated_method is Hello class is removed but the unit test still passes without spec
+        mock = Mock()
+        Hello.deprecated_method = Mock
+        mock.deprecated_method()
+        self.assertTrue(Hello.deprecated_method.called)
+
     def test_mock_with_spec(self):
         """ spec ensures mock to track class method """
-        mock = Mock()
-        mock.deprecated_method() # this doesn't raise exception, which is bad
-
         mock = Mock(spec=Hello)
         with self.assertRaises(AttributeError):
             mock.deprecated_method()
 
     @patch("hello.Hello.greet")  # func must be on path. "Hello.great" doesn't work since import has no effect.
     def test_greet_with_patch(self, mock_result):
-        mock_result.return_value = "it's mocked"
-        self.assertEqual(Hello.greet("John"), "it's mocked")
+        # test patch the return value
+        mock_result.return_value = "hey, I am mock."
+        self.assertEqual(Hello.greet("John"), "hey, I am mock.")
 
     @patch("hello.Hello.greet")
     def test_greet_with_patch2(self, mock_result):
+        # test patch with side effect
         mock_result.side_effect = ["mock", "you"]
         self.assertEqual(Hello.greet("John"), "mock")
         self.assertEqual(Hello.greet("John"), "you")
 
     @patch("hello.Hello.greet", return_value="mocked again") # you can put return_value or side_effect in decorator
     def test_greet_with_patch3(self, mock_call):
+        # another way to patch return value
         self.assertEqual(Hello.greet("John"), "mocked again")
 
     def test_greet_with_patch_context(self):
-        """ use with can avoid multiple decorators """
+        """ use context manager to avoid multiple decorators """
         with patch('hello.Hello.greet') as mock_greet:
             mock_greet.return_value = "mocking"
             self.assertEqual(Hello.greet("John"), "mocking")
 
-    @patch("hello.Hello.guess_a_number")
-    def test_guess_a_number(self, mock_result):
+    @patch("hello.randint")
+    def test_mock_randint(self, mock_result):
+        # this is the right way to patch
         mock_result.return_value = 777
         self.assertEqual(Hello.guess_a_number(), 777)
 
     @patch("random.randint")
-    def test_mock_randint(self, mock_result):
-        # note it is a common mistake to patch the local random.randint instead of the one in hello module !!!
+    def test_mock_randint2(self, mock_result):
+        # want to mock function from another module, but mock local one instead -- common mistake !!!
         mock_result.return_value = 777
-        # we didn't patch that one
         self.assertNotEqual(Hello.guess_a_number(), 777)
-        # we patched this local one
         self.assertEqual(random.randint(0, 10), 777)
 
-    @patch("hello.randint")
-    def test_mock_randint2(self, mock_result):
-        # this is the right way to patch
-        mock_result.return_value = 777
-        self.assertEqual(Hello.guess_a_number(), 777)
+
